e1d849e85a3da74e8f30de17c0221978fdfd627c
Andy Duverneau <77910275+andydu001@users.noreply.github.com>
2024-10-07 01:41:01 -0400

    Add files via upload

---
+++ IDada.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Data;
+ 
+namespace Andy
+{
+    internal class IDada : IDataAdapter
+    {
+        public MissingMappingAction MissingMappingAction { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public MissingSchemaAction MissingSchemaAction { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public ITableMappingCollection TableMappings => throw new NotImplementedException();
+
+        public int Fill(DataSet dataSet)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DataTable[] FillSchema(DataSet dataSet, SchemaType schemaType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDataParameter[] GetFillParameters()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Update(DataSet dataSet)
+        {
+            if (dataSet.HasChanges())
+            {
+                
+                dataSet.Merge(dataSet);
+                
+            }
+            
+            return dataSet.Tables.Count;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,93 @@
+﻿/*
+ * 
+ * This program shows how a key_pair value can be created
+ * and how a file can be manipulated in c#
+ * 
+ * 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+
+namespace Andy
+{
+    internal class Program
+    {
+        private const int V = 3;
+
+        static void Main(string[] args)
+        {
+            var key_Val = new KeyValuePair<string, int>("Andy", 8);
+
+            Console.WriteLine(key_Val.Key);
+
+            
+            var ida = new IDada();
+            
+            var dataset = new DataSet();
+
+           
+
+            var dataTable = new    DataTable();
+
+             _ =  dataTable.Columns.Add("Cars").ColumnName;
+             _ = dataTable.Columns.Add("Carise").ColumnName;
+             _ = dataTable.Columns.Add("Car").ColumnName;
+
+            var da = dataTable.NewRow() ;
+            
+
+ 
+
+            da.SetField(0,"Ann fe ");
+            da.SetField(1, "bag");
+ 
+
+   
+               int item = da.ItemArray.Length;
+
+               for(int i = 0; i < item; i++)
+               {
+
+                Console.WriteLine(da.ItemArray[i]);
+               }
+
+                   
+                 dataset.Tables.Add(dataTable);
+       
+
+            Console.WriteLine(ida.Update(dataset));
+
+           // var file = File.OpenRead("C:\\Users\\andyd\\OneDrive\\Documents\\CADA.txt");
+            try
+            {
+                var file = File.OpenRead("C:\\Users\\andyd\\OneDrive\\Documents\\CADA.txt");
+                while (file.Position < file.Length)
+                {
+                    var ch = (char)file.ReadByte();
+
+                    if (ch.ToString().Equals(ch.ToString().ToUpper()))
+                    {
+
+                        Console.Write(ch);
+                    }
+
+                    // Console.Write(((char)file.ReadByte()));
+
+                }
+                file.Close();
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine ("File doesn't exist");
+            }
+                 
+            
+         
+
+        }
+    }
+}
